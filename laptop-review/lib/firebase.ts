d7f89a5d5610a25e5b8e6efab4eab82e9ae94b41
Tăng Thoại Hào <haotang.31231027959@st.ueh.edu.vn>
2025-05-07 17:43:42 +0700

    hide the API

--- laptop-review/lib/firebase.ts
+++ laptop-review/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore, doc, setDoc, getDoc, updateDoc, arrayUnion, arrayRemove }
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAFSVL94k5zXkrAy5oQKbO7rT6W5fPAk4M",
-  authDomain: "laptop-review-all.firebaseapp.com",
-  projectId: "laptop-review-all",
-  storageBucket: "laptop-review-all.firebasestorage.app",
-  messagingSenderId: "1044782876129",
-  appId: "1:1044782876129:web:6e0891bf2753c5a3f63ea0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
