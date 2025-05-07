501e65384ac1ef31b645fabd2e30913cd2dc2379
HP <nahmsix2005@gmail.com>
2025-05-07 08:35:51 +0700

    import functions from Firestore

--- laptop-review/firebase.js
+++ laptop-review/firebase.js
@@ -2,6 +2,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,GoogleAuthProvider,signInWithPopup   } from "firebase/auth";
 
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
